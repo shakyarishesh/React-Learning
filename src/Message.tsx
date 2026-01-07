2431b59baaa8bc8c24c56e01f88401f5eecbaabb
shakyarishesh <shakyarises1211@gmail.com>
2026-01-07 23:32:09 +0545

    feat: initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+  const name = "Rishesh";
+  //   const name = null;
+  if (name) 
+    return <h1>Hello {name}. How are you</h1>;
+  return <h1>Hello aaru</h1>;
+}
+
+export default Message;
