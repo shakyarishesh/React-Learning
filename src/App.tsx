57183ef7c8adff2189ddbd18d12fe19992743e13
shakyarishesh <shakyarises1211@gmail.com>
2026-02-01 16:24:24 +0545

    feat: passing a children via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Apple", "Banana", "Grape", "Avacodo"];
-
-  let count = 1;
-
-  const handleOnSelectItem = (item: string, value: number) => {
-    console.log(item + ':' + value++);
-  }
-
   return (
     <div>
-      <ListGroup items={items} heading="Fruits" count={count} onSelectItem={handleOnSelectItem}></ListGroup>
+      <Alert>
+        Hello this is <span>Rishesh</span>.
+      </Alert>
     </div>
   );
 }
