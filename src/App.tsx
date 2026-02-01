0d4ab0489a7218fce0bd116acde820f63a9ae1b6
shakyarishesh <shakyarises1211@gmail.com>
2026-02-01 15:44:59 +0545

    feat: passing the function via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import ListGroup from './components/ListGroup';
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup></ListGroup></div>
+  let items = ["Apple", "Banana", "Grape", "Avacodo"];
+
+  let count = 1;
+
+  const handleOnSelectItem = (item: string, value: number) => {
+    console.log(item + ':' + value++);
+  }
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Fruits" count={count} onSelectItem={handleOnSelectItem}></ListGroup>
+    </div>
+  );
 }
 
 export default App;
