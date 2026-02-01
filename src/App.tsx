5922e1baa9fb78ccc3053497ccc1290b3f4c3f3e
shakyarishesh <shakyarises1211@gmail.com>
2026-02-01 22:37:51 +0545

    feat: exercse where on clicking the button the alert will be shown and on clicking the cross button the alert will be closed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <div>
-      <Button color="primary" onClick={() => console.log('clicked.')}>
+      {showAlert && <Alert onclose={() => setShowAlert(false)}> This is an alert. </Alert>}
+      <Button color="primary" onClick={() => setShowAlert(true) }>
         My button
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,12 +1,25 @@
 import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
+  onclose: () => void;
 }
-const Alert = ({children}: Props) => {
+const Alert = ({ children, onclose }: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-  )
-}
+    <div
+      className="alert alert-primary alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
 
-export default Alert
+      <button
+        type="button"
+        className="btn-close"
+        aria-label="Close"
+        onClick={onclose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
